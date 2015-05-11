3ab1c9b441905885c93616844cd20a9de987ae00
Walid Choufani <walidchoufani@users.noreply.github.com>
2015-05-11 14:57:32 +0300

    akel hawa2

---
+++ store1/scripts/test/script1.js
@@ -0,0 +1,11 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ var maskhara = "akel hawa";
+/*
+* to note that this a test added comment.......
+*/
+
+return "test/script1 is " + maskhara ;   				   							
