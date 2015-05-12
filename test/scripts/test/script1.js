62b8091f612900e6e706963c3c4799eb4cbfd836
Walid Choufani <walidchoufani@users.noreply.github.com>
2015-05-12 17:15:45 +0300

    asd

--- test/scripts/test/script1.js
+++ test/scripts/test/script1.js
@@ -6,6 +6,6 @@ execute=authenticated
  var ssss = "smr3salsslsd emdf, aktar men heik akel haswa w maskhara ma 3ed ysir";
 
 /*
-* no comments, i said dno comments
+* no comments, i sdaid dno comments
 */
 return "ktir hal2ad " + ssss;				   				   				   				   							
