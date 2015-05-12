68457cf74cd695e60bd2f42d444948f5888ea2f4
Walid Choufani <walidchoufani@users.noreply.github.com>
2015-05-12 17:13:03 +0300

    sdad

--- test/scripts/test/script1.js
+++ test/scripts/test/script1.js
@@ -3,7 +3,7 @@
 write=nobody
 execute=authenticated 
   **/ 
- var ahra664ra = "m3allemdf, aktar men heik akel hawa w maskhara ma 3ed ysir";
+ var ahra664ra = "mr3allemdf, aktar men heik akel hawa w maskhara ma 3ed ysir";
 
 /*
 * no comments, i said no comments
